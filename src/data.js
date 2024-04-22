aa1e0fa59efca70342aaf2da3acfdfb85af8ea70
satyam pandey <119337626+isatyam05102003@users.noreply.github.com>
2024-04-23 02:01:18 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,98 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+},
+    {
+        "id": 1,
+        "title": "Samsung Galaxy S22",
+        "price": 36530, // Converted price to INR
+        "description": "Samsung Galaxy S22 price in India is ₹36530. Official dealers and warranty providers regulate the retail price of Samsung mobile products in official warranty.",
+        "brand": "Samsung",
+        "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS22-b.jpg",
+        "link":"https://amzn.to/3xCZphh"
+    },
+    {
+        "id": 2,
+        "title": "Vivo X80",
+        "price": 75999, // Converted price to INR
+        "description": "Vivo X80 price in India is ₹75,999. Official dealers and warranty providers regulate the retail price of Vivo mobile products in official warranty.",
+        "brand": "Vivo",
+        "image": "https://www.whatmobile.com.pk/admin/images/Vivo/VivoX80-b.jpg",
+        "link":"https://amzn.to/3xE93jS"
+    },
+    {
+        "id": 3,
+        "title": "Oppo F21 Pro 5G",
+        "price": 41999, // Converted price to INR
+        "description": "Oppo F21 Pro 5G price in India is ₹41,999. Official dealers and warranty providers regulate the retail price of Oppo mobile products in official warranty.",
+        "brand": "Oppo",
+        "image": "https://www.whatmobile.com.pk/admin/images/Oppo/OppoF21Pro5G-b.jpg",
+        "link": "https://amzn.to/3TRh0JM"
+    },
+    {
+        "id": 4,
+        "title": "Infinix Note 12",
+        "price": 10999, // Converted price to INR
+        "description": "Infinix Note 12 price in India is ₹10,999. Official dealers and warranty providers regulate the retail price of Infinix mobile products in official warranty.",
+        "brand": "Infinix",
+        "image": "https://www.whatmobile.com.pk/admin/images/Infinix/InfinixNote12-b.jpg",
+        "link":"https://amzn.to/3TVKeXW"
+    }
+,
+    {
+        "id": 5,
+        "title": "Samsung Galaxy A13",
+        "price": 14990, // Converted price to INR
+        "description": "Samsung Galaxy A13 price in India is ₹14,990. Official dealers and warranty providers regulate the retail price of Samsung mobile products in official warranty.",
+        "brand": "Samsung",
+        "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyA13-b.jpg",
+        "link":"https://amzn.to/3TVZunM"
+    },
+    {
+        "id": 6,
+        "title": "Xiaomi 12",
+        "price": 9975, // Converted price to INR
+        "description": "Xiaomi 12 price in India is ₹9,975. Official dealers and warranty providers regulate the retail price of Xiaomi mobile products in official warranty.",
+        "brand": "Xiaomi",
+        "image": "https://www.whatmobile.com.pk/admin/images/Xiaomi/Xiaomi12-b.jpg",
+        "link":"https://amzn.to/3JjRwjw"
+    },
+    {
+        "id": 7,
+        "title": "Huawei Nova 9",
+        "price": 50999, // Converted price to INR
+        "description": "Huawei Nova 9 price in India is ₹50,999. Official dealers and warranty providers regulate the retail price of Huawei mobile products in official warranty.",
+        "brand": "Huawei",
+        "image": "https://www.whatmobile.com.pk/admin/images/Huawei/HuaweiNova9-b.jpg"
+    },
+    {
+        "id": 8,
+        "title": "Realme C35",
+        "price": 8799, // Converted price to INR
+        "description": "Realme C35 price in India is ₹8,799. Official dealers and warranty providers regulate the retail price of Realme mobile products in official warranty.",
+        "brand": "Realme",
+        "image": "https://www.whatmobile.com.pk/admin/images/Realme/RealmeC35-b.jpg",
+        "link":"https://amzn.to/3xABRcW"
+    },
+    {
+        "id": 9,
+        "title": "Tecno Spark 8C 3GB",
+        "price": 8999, // Converted price to INR
+        "description": "Tecno Spark 8C 3GB price in India is ₹8,999. Official dealers and warranty providers regulate the retail price of Tecno mobile products in official warranty.",
+        "brand": "Tecno",
+        "image": "https://www.whatmobile.com.pk/admin/images/Tecno/TecnoSpark8C3GB-b.jpg",
+        "link":"https://amzn.to/4cVF8DB"
+    },
+    {
+        "id": 10,
+        "title": "itel A70",
+        "price": 6799, // Converted price to INR
+        "description": "itel Vision 3 price in India is ₹6,799. Official dealers and warranty providers regulate the retail price of itel mobile products in official warranty.",
+        "brand": "Itel",
+        "image": "https://www.whatmobile.com.pk/admin/images/itel/itelVision3-b.jpg",
+        "link":"https://amzn.to/3Q24SEu"
+    }]
